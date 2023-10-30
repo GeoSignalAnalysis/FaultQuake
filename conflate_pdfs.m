312fb70522aae8e490061433d31238f543d2f915
Nasrin Tavakolizadeh <128606941+GeoSignalAnalysis@users.noreply.github.com>
2023-10-30 22:03:51 +0000

    Delete conflate_pdfs.m

--- conflate_pdfs.m
+++
@@ -1,9 +0,0 @@
-function conflated = conflate_pdfs(x, pdfs)
-    conflated = ones(size(x)) / (x(end) - x(1));
-    for i = 1:length(pdfs)
-        pdf = pdfs{i} / trapz(x, pdfs{i});
-        conflated = conflated .* pdf;
-    end
-    conflated = conflated / trapz(x, conflated);
-end
-
